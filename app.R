309cc4d861f31a536ec551f373ed12efc2bf1727
Rolf Marvin Bøe Lindgren <rolf@grendel.no>
2025-12-02 22:34:42 +0100

    multilingual

--- app.R
+++ app.R
@@ -7,115 +7,145 @@ library(shiny.i18n)
 
 options(bslib.cache = FALSE)
 
-i18n <- Translator$new(translation_csvs_path = "../data")
+i18n <- Translator$new(translation_csvs_path =
+                         Sys.getenv("ADHD_DB_PATH"),
+                       translation_csv_config=
+                         paste0(Sys.getenv("ADHD_DB_PATH"),"config.yml"))
 
-## Spørsmål
-items <- tibble(
-  id = paste0("item", 1:10),
-  tekst = c(
-    "Jeg klarer vanligvis å starte på kjedelige oppgaver uten særlig friksjon.",
-    "Jeg mister sjelden viktige ting, og finner dem raskt hvis det skjer.",
-    "Jeg blir stort sett oppfattet som pålitelig når det gjelder avtaler og frister.",
-    "Jeg klarer som regel å holde fokus på én oppgave til den er fullført.",
-    "Jeg er ikke avhengig av kaos eller siste liten for å komme i gang.",
-    "Jeg blir ikke så rastløs at jeg må bevege meg eller avbryte andre.",
-    "Jeg glemmer ikke avtaler eller beskjeder i en grad som skaper problemer.",
-    "Når noe er viktig, klarer jeg å organisere tiden så det får plass.",
-    "Tankene løper ikke så ofte løpsk at det hindrer meg i hverdagen.",
-    "Jeg trenger ikke uvanlig mye ytre struktur for å fungere greit."
-  )
-)
+i18n$set_translation_language("nb")
 
 ui <- fluidPage(
+  
+  usei18n(i18n),
   useShinyjs(),
   tags$head(
          tags$link(rel = "stylesheet", type = "text/css", href = "custom.css")
        ),
   theme = bslib::bs_theme(version = 5, bootswatch = "flatly"),
-  titlePanel("Dette er ikke en ADHD-test"),
+  titlePanel(i18n$t("Dette er ikke en ADHD-test")),
 
   sidebarLayout(
     sidebarPanel(
-      h4("Kort om testen"),
-      p("Denne testen viser hvordan fravær av oppmerksomhets- og reguleringsvansker kan se ut."),
-      p("Den er ikke diagnostisk. Den kan verken bekrefte eller avkrefte ADHD."),
+      selectInput(
+        inputId = "selected_language",
+        label = i18n$t(i18n$t("Skift språk")),
+        choices = setNames(
+          i18n$get_languages(),
+          i18n$get_languages()
+
+        ),
+        selected = i18n$get_key_translation()
+      ),
+
+      h4(i18n$t("Kort om testen")),
+      p(i18n$t("Denne testen viser hvordan fravær av oppmerksomhets- og reguleringsvansker kan se ut.")),
+      p(i18n$t("Den er ikke diagnostisk. Den kan verken bekrefte eller avkrefte ADHD.")),
       br(),
-      actionButton("beregn", "Beregn resultat")
+      actionButton("beregn", i18n$t("Beregn resultat"))
     ),
 
     mainPanel(
       tabsetPanel(id="tabs",
                   tabPanel(
-                    "Spørsmål",
+                    i18n$t("Spørsmål"),
                     br(),
-                    p("Dra i hver slider for å velge hvor godt utsagnet har stemt for deg over tid."),
+                    p(i18n$t("Dra i hver slider for å velge hvor godt utsagnet har stemt for deg over tid.")),
                     hr(),
+                    uiOutput("sporsmals_ui")
 
-                    lapply(seq_len(nrow(items)), function(i) {
-                      item <- items[i, ]
-
-                      ## slider + labels får felles ramme
-                      div(style = "width: 100%; max-width: 600px;",
-
-                          tagList(
-                            strong(item$tekst),
-
-                            div(class = "scale-labels",
-                                HTML("
-        <span>Stemmer ikke</span>
-        <span>Litt</span>
-        <span>Delvis</span>
-        <span>Ganske</span>
-        <span>Stemmer helt</span>
-    ")
-    ),
-
-    div(style = "width: 100%;",
-        sliderInput(
-          inputId = item$id,
-          label = NULL,
-          min = 1,
-          max = 5,
-          value = 3,
-          step = 1,
-          ticks = FALSE,
-          width = "100%"
-        )
-        ),
-
-    br()
-    )
-    )
-                    })
-    ),
-    tabPanel(
-      "Resultat",
-      br(),
-      h3("Tolkning"),
-      textOutput("resultat_tekst"),
-      br(),
-      h4("Gjennomsnittsskår"),
-      textOutput("score_tekst"),
-      br(),
-      h4("Forbehold"),
-      p("En klinisk vurdering innebærer utviklingshistorie, funksjon og faglig skjønn."),
-      p("Mennesker kan ha lav struktur eller høy fart uten at det handler om ADHD.")
-    )
-    )
+                  ),
+                  tabPanel(
+                    i18n$t("Resultat"),
+                    br(),
+                    h3(i18n$t("Tolkning")),
+                    textOutput("resultat_tekst"),
+                    br(),
+                    h4(i18n$t("Gjennomsnittsskår")),
+                    textOutput("score_tekst"),
+                    br(),
+                    h4(i18n$t("Forbehold")),
+                    p(i18n$t("En klinisk vurdering innebærer utviklingshistorie, funksjon og faglig skjønn.")),
+                    p(i18n$t("Mennesker kan ha lav struktur eller høy fart uten at det handler om ADHD."))
+                  )
+                  )
     )
   )
 )
 
 server <- function(input, output, session) {
 
+  items_r <- reactive({
+
+    lang <- input$selected_language   # <- gjør reactive() avhengig av språket
+    i18n$set_translation_language(lang)
+
+    tibble(
+      id = paste0("item", 1:10),
+      tekst = c(
+        i18n$t("Jeg klarer vanligvis å starte på kjedelige oppgaver uten særlig friksjon"),
+        i18n$t("Jeg mister sjelden viktige ting, og finner dem raskt hvis det skjer"),
+        i18n$t("Jeg blir stort sett oppfattet som pålitelig når det gjelder avtaler og frister"),
+        i18n$t("Jeg klarer som regel å holde fokus på én oppgave til den er fullført"),
+        i18n$t("Jeg er ikke avhengig av kaos eller siste liten for å komme i gang"),
+        i18n$t("Jeg blir ikke så rastløs at jeg må bevege meg eller avbryte andre"),
+        i18n$t("Jeg glemmer ikke avtaler eller beskjeder i en grad som skaper problemer"),
+        i18n$t("Når noe er viktig, klarer jeg å organisere tiden så det får plass"),
+        i18n$t("Tankene løper ikke så ofte løpsk at det hindrer meg i hverdagen"),
+        i18n$t("Jeg trenger ikke uvanlig mye ytre struktur for å fungere greit")
+      )
+    )
+  })
+
+
+  output$sporsmals_ui <- renderUI({
+    items <- items_r()   # <- reaktiv tibble
+    lapply(seq_len(nrow(items)), function(i) {
+      item <- items[i, ]
+
+      div(style = "width: 100%; max-width: 600px;",
+          strong(item$tekst),
+          div(class = "scale-labels",
+              HTML(sprintf(
+                "<span>%s</span>
+     <span>%s</span>
+     <span>%s</span>
+     <span>%s</span>
+     <span>%s</span>",
+     i18n$t("Stemmer ikke"),
+     i18n$t("Litt"),
+     i18n$t("Delvis"),
+     i18n$t("Ganske"),
+     i18n$t("Stemmer helt")
+     ))
+     ),
+     sliderInput(
+       inputId = item$id,
+       label = NULL,
+       min = 1,
+       max = 5,
+       value = 3,
+       step = 1,
+       ticks = FALSE,
+       width = "100%"
+     ),
+     br()
+     )
+    })
+  })
+
+
   db_path <- Sys.getenv(paste0("ADHD_DB_PATH","ADHD_DB_NAME"))
 
-
+  observeEvent(input$selected_language, {
+    i18n$set_translation_language(input$selected_language)
+    update_lang(language=input$selected_language,session=session)
+    print(input$selected_language)
+  })
 
   observeEvent(input$beregn, {
 
     shinyjs::disable("beregn")
-    shinyjs::html("beregn", "Sendt ✔")
+    shinyjs::html("beregn", paste0(i18n$t("Sendt"), "✔"))
 
     ## (valgfritt) re-enable etter 5 sekunder
     shinyjs::delay(5000, shinyjs::enable("beregn"))
@@ -123,7 +153,7 @@ server <- function(input, output, session) {
 
     timestamp <- format(Sys.time(), "%Y-%m-%d %H:%M:%S")
 
-    item_ids <- items$id   # ta ID-er fra tibblen, ikke fra input
+    item_ids <- items_r()$id   # ta ID-er fra tibblen, ikke fra input
 
     scores <- vapply(item_ids, function(id) input[[id]], numeric(1))
 
@@ -135,10 +165,9 @@ server <- function(input, output, session) {
 
     dbWriteTable(con, "responses", df, append = TRUE)
 
-
     dbDisconnect(con)
 
-    updateTabsetPanel(session, "tabs", selected = "Resultat")
+    updateTabsetPanel(session, "tabs", selected = i18n$t("Resultat"))
 
   })
 
@@ -147,8 +176,9 @@ server <- function(input, output, session) {
 
   score_reaktiv <- eventReactive(input$beregn, {
 
+
     ## hent alle svar som liste
-    svar <- lapply(items$id, function(id) input[[id]])
+    svar <- lapply(items_r()$id, function(id) input[[id]])
 
     ## sjekk ubesvarte
     mangler <- vapply(svar, function(x) is.null(x) || is.na(x), logical(1))
@@ -156,12 +186,12 @@ server <- function(input, output, session) {
     if (any(mangler)) {
       return(list(
         gyldig = FALSE,
-        beskjed = "Du må svare på alle utsagn før resultatet kan beregnes.",
+        beskjed = i18n$t("Du må svare på alle utsagn før resultatet kan beregnes."),
         score = NA
       ))
     }
 
-                                        # flate ut og beregne
+    ## flate ut og beregne
     svar_num <- as.numeric(unlist(svar))
     mean_score <- mean(svar_num)
 
@@ -175,8 +205,9 @@ server <- function(input, output, session) {
   output$score_tekst <- renderText({
     res <- score_reaktiv()
     if (!res$gyldig) return(res$beskjed)
-    paste0(round(res$score, 2), " av 5")
+    paste0(round(res$score, 2), i18n$t(" av 5"))
   })
+
 
   output$resultat_tekst <- renderText({
     res <- score_reaktiv()
@@ -185,13 +216,13 @@ server <- function(input, output, session) {
     s <- res$score
 
     if (s >= 4.0) {
-      "Svarene dine viser få trekk som ligner de reguleringsvanskene man ser ved ADHD. Hverdagen virker stabil og forutsigbar."
+      i18n$t("Svarene dine viser få trekk som ligner de reguleringsvanskene man ser ved ADHD. Hverdagen virker stabil og forutsigbar.")
     } else if (s >= 3.2) {
-      "Mønsteret ditt ligger godt innenfor normal variasjon: noen styrker, litt friksjon, men ingenting som peker klart i én retning."
+      i18n$t("Mønsteret ditt ligger godt innenfor normal variasjon: noen styrker, litt friksjon, men ingenting som peker klart i én retning.")
     } else if (s >= 2.5) {
-      "Du rapporterer en del trekk som kan minne om ADHD, men dette kan like gjerne handle om personlighet, vaner eller livssituasjon."
+      i18n$t("Du rapporterer en del trekk som kan minne om ADHD, men dette kan like gjerne handle om personlighet, vaner eller livssituasjon.")
     } else {
-      "Du beskriver flere områder som ofte skaper vansker i ADHD. Dette er fortsatt ikke diagnostikk, men det kan være verdt en mer formell vurdering dersom dette skaper problemer i hverdagen."
+      i18n$t("Du beskriver flere områder som ofte skaper vansker i ADHD. Dette er fortsatt ikke diagnostikk, men det kan være verdt en mer formell vurdering dersom dette skaper problemer i hverdagen.")
     }
   })
 }
